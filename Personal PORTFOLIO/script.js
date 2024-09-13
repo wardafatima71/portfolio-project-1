151624623923f0b4de580a9a88b4f4b5032916cb
warda <173349808+wardafatima71@users.noreply.github.com>
2024-09-13 20:27:11 +0500

    Add files via upload

---
+++ Personal PORTFOLIO/script.js
@@ -0,0 +1,7 @@
+// JavaScript to toggle the mobile menu
+//  const toggleButton = document.querySelector('.navbar-toggle');
+//  const nav = document.querySelector('nav');
+
+//  toggleButton.addEventListener('click', () => {
+//    nav.classList.toggle('active'); // Toggle the 'active' class
+//  });
